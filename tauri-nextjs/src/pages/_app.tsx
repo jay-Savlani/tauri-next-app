c2d4f7128e5c1d9ca719e56721bd5cb36277c6b8
Jay Savlani <jaysavlani2608@gmail.com>
2022-10-30 01:36:30 +0530

    added todo app functionality

--- tauri-nextjs/src/pages/_app.tsx
+++ tauri-nextjs/src/pages/_app.tsx
@@ -1,9 +1,13 @@
 import type { AppProps } from "next/app";
+import "bootstrap/dist/css/bootstrap.css";
 
-import "../style.css";
-import "../App.css";
+import TasksProvider from "../context_providers/TasksProvider";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <TasksProvider>
+      <Component {...pageProps} />
+    </TasksProvider>
+  );
 }
